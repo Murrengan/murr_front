c50b6101401ad533acf2f97bdde9e2dd0105974e
Dreiz Yuri <solomonfx24@gmail.com>
2020-02-23 22:08:16 +0200

    fixed notify - you must login

--- src/router.js
+++ src/router.js
@@ -1,85 +1,73 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
 import store from '@/store/index';
-
 
 Vue.use(Router);
 
 const router = new Router({
-    mode: 'history',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            meta: {layout: 'main-layout', murr_some_data: '42'},
-            component: () => import('./views/Home.vue')
-        },
-        {
-            path: '/login',
-            name: 'login',
-            meta: {layout: 'empty-layout'},
-            component: () => import('./views/murren/Login.vue')
-        },
-        {
-            path: '/signup',
-            name: 'signup',
-            meta: {layout: 'empty-layout'},
-            component: () => import('./views/murren/SignUp.vue')
-        },
-        {
-            path: '/murren',
-            name: 'murren',
-            meta: {layout: 'main-layout', accessTokenExpected: true},
-            component: () => import('./views/murren/Murren.vue')
-        },
-        {
-            path: '/check_email',
-            name: 'check_email',
-            meta: {layout: 'main-layout'},
-            component: () => import('./views/murren/CheckEmail.vue')
-        },
-        {
-            path: '/murren_email_activate',
-            name: 'murren_email_activate',
-            meta: {layout: 'main-layout'},
-            component: () => import('./views/murren/ConfirmEmail.vue')
-        },
-        {
-            path: '/set_new_password',
-            name: 'set_new_password',
-            meta: {layout: 'main-layout'},
-            component: () => import('./views/murren/SetNewPassword.vue')
-        },
-    ]
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      meta: {layout: 'main-layout', murr_some_data: '42'},
+      component: () => import('./views/Home.vue'),
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {layout: 'empty-layout'},
+      component: () => import('./views/murren/Login.vue'),
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      meta: {layout: 'empty-layout'},
+      component: () => import('./views/murren/SignUp.vue'),
+    },
+    {
+      path: '/murren',
+      name: 'murren',
+      meta: {layout: 'main-layout', accessTokenExpected: true},
+      component: () => import('./views/murren/Murren.vue'),
+    },
+    {
+      path: '/check_email',
+      name: 'check_email',
+      meta: {layout: 'main-layout'},
+      component: () => import('./views/murren/CheckEmail.vue'),
+    },
+    {
+      path: '/murren_email_activate',
+      name: 'murren_email_activate',
+      meta: {layout: 'main-layout'},
+      component: () => import('./views/murren/ConfirmEmail.vue'),
+    },
+    {
+      path: '/set_new_password',
+      name: 'set_new_password',
+      meta: {layout: 'main-layout'},
+      component: () => import('./views/murren/SetNewPassword.vue'),
+    },
+  ],
 });
-
-import {Message} from 'element-ui';
 
 router.beforeEach((to, from, next) => {
 
-    if (to.matched.some(record => record.meta.accessTokenExpected)) {
+  const isTokenExpectedAndExist = (to.matched.some(r => r.meta.accessTokenExpected) && store.state.auth.accessToken) ||
+    !to.matched.some(r => r.meta.accessTokenExpected);
 
-        if (!store.getters.accessToken_getters) {
+  if (isTokenExpectedAndExist) {
+    return next();
+  }
 
-            store.dispatch('changeShowLoginForm_actions');
+  store.dispatch('popUpMessage', {
+    message: 'Требуется авторизация 😳',
+    type: 'warning',
+  });
 
-            Message({
-                message: 'Требуется авторизация 😳',
-                center: true,
-                customClass: 'element-ui-message__error',
-                type: 'warning',
-                showClose: true
-            });
-
-        } else {
-
-            next()
-        }
-    } else {
-
-        next()
-    }
+  store.dispatch('changeShowLoginForm_actions');
 });
 
-export default router
+export default router;
