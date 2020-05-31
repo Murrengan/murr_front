5c40103b5d80c127327a1e9c3c31354b5138b91c
Murrengan <egor.a.komarov@yandex.ru>
2020-05-31 21:25:06 +0300

    change axios_defaults_baseURL to 127 and 80 port

--- src/devAndProdVariables.js
+++ src/devAndProdVariables.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 // dev
-export const axios_defaults_baseURL = 'http://127.0.0.1:8000';
+export const axios_defaults_baseURL = 'http://127.0.0.1';
 
 // prod
 // export const axios_defaults_baseURL = 'https://murrengan.ru';
